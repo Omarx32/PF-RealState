ba4576ef0e2ea618e1e6a25e810569eb2319035e
AbareKiller100 <themr.camper213@gmail.com>
2023-09-28 21:48:32 -0300

    Filtrado por locación

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const server = require("./src/app.js")
 const {conn} = require ("./src/db.js")
 const {PORT} = process.env
 
-const engine= require('ejs-mate'); 
-const path= require('path')
+// const engine= require('ejs-mate'); 
+// const path= require('path')
 
-server.engine('ejs', engine)
-server.set('view engine', 'ejs')
-server.set('views', path.join(__dirname, 'views'));
-console.log(__dirname);
+// server.engine('ejs', engine)
+// server.set('view engine', 'ejs')
+// server.set('views', path.join(__dirname, 'views'));
+// console.log(__dirname);
 
-server.use(require('./routes/'))
+// server.use(require('./routes/'))
 
 conn.sync({ force: false }).then(() => {
     server.listen(PORT, () => {
