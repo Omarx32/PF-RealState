406a6fb76dcb8db642bf7c3680c5b24de9c10c16
Santiago Ramirez <santiagoramirescardona@gmail.com>
2023-10-03 12:29:27 -0500

    Notificaciones

--- src/controllers/PropertyC/CreateNewRent.js
+++ src/controllers/PropertyC/CreateNewRent.js
@@ -14,28 +14,6 @@ async function createProperty(form) {
     //console.log(input);
     //console.log(input);
 
-<<<<<<< HEAD
-        const{
-           title,
-           description,
-           image,
-           numBeds,
-           numBaths,
-           nightPrice,
-           availability,
-           homeCapacity,
-        }= input
-
-        if (!title || !description || !image || !numBaths || !numBeds || !nightPrice || !availability || !homeCapacity){
-            throw new Error("Missing required data")
-        }
-        const imageUrls = [];
-        for (const imageData of image){
-            const result = await cloudinary.uploader.upload(imageData,{
-                folder: "productsDetail"
-            });
-            imageUrls.push(result.secure_url)
-=======
     const {
         title,
         description,
@@ -67,30 +45,11 @@ async function createProperty(form) {
         const category = await Category.findOne({ where: { name: categorys } });
         if (!categorys) {
             throw new Error(`Category "${category}" doesn't exist`)
->>>>>>> master
         }
         await createdProperty.setCategory(category)
     }
 
-<<<<<<< HEAD
-        const newProperty = {title, description, image: imageUrls, numBaths, numBeds, nightPrice, availability, homeCapacity}
-
-        const createdProperty = await Property.create(newProperty)
-
-        const categorys = input.Category;
-
-        if(categorys){
-            const category = await Category.findOne({where: {name: categorys} });
-            if(!category){
-                throw new Error(`Category "${category}" doesn't exist`)
-            }
-            await createdProperty.setCategory(category)
-        }
-
-        return createdProperty
-=======
     return createdProperty
->>>>>>> master
 }
 
 module.exports = createProperty;
