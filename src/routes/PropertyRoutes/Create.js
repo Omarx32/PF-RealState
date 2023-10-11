c059dce48411e4996be8689d114a64890350fcb3
Santiago Ramirez <santiagoramirescardona@gmail.com>
2023-10-11 15:38:35 -0500

    Admin property

--- src/routes/PropertyRoutes/Create.js
+++ src/routes/PropertyRoutes/Create.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const createPropertyH = require('../../handlers/PropertyH/CreateNewRentH')
 const {getProperty, getPropertybyId} = require('../../handlers/PropertyH/PropertyHand')
+const {PropertyAdmin} = require("../../controllers/PropertyC/PropertyAdmin")
 
 
 const router = express.Router()
 
 router.get('/gproduct', getProperty)
 router.get('/:id', getPropertybyId)
-router.post('/post', createPropertyH);
+router.post('/post', createPropertyH)
+router.put("/gproductAdmin/:id", PropertyAdmin)
 
 
 
