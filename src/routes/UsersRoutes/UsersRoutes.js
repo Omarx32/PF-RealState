cb7715ff741c8c5ef61e89ecb381ec69b2970774
fabian <fasalpe34@gmail.ocm>
2023-10-11 14:52:46 -0500

    ruta- getUsers

--- src/routes/UsersRoutes/UsersRoutes.js
+++ src/routes/UsersRoutes/UsersRoutes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const { createUserHandler } = require('../../handlers/UserHandler/UserHandler'); 
 const {loginUsers} = require('../../controllers/Users/LoginUsers')
 const {userGoogle}=require ('../../controllers/Users/loginUsersGoogle')
+const{usersGet}= require ("../../controllers/GetUsers/usersGet")
 
 const router = express.Router();
 
 router.post('/create', createUserHandler);
 router.post('/login', loginUsers)
 router.post('/googleLogin',userGoogle)
+router.get ("/userget", usersGet)
 
 module.exports = router;
