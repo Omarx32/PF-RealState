52fb0353073baa639edde9fff888bd90df175c50
fabian <fasalpe34@gmail.ocm>
2023-10-12 14:53:45 -0500

    controller dashboardAllstatistiscs

--- src/routes/DashboardRentRoutes/DashboardCreated.js
+++ src/routes/DashboardRentRoutes/DashboardCreated.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { getMonthlyStatistics } = require('../../handlers/DashBoardHandler/DashBoardHandler') 
+const {dashboardAllRentsHandler} = require ('../../handlers/DashboardAllRentsHandler/DashboardAllRentsH')
 const router = express.Router();
 
 router.get('/statistics/:month', getMonthlyStatistics);
+router.get('/Allstatistics',dashboardAllRentsHandler);
 
 module.exports = router;
