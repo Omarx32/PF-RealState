7588a560414b7ef1030debc6cec66603c62ddd3b
Santiago Ramirez <santiagoramirescardona@gmail.com>
2023-10-13 11:22:59 -0500

    Login Admin

--- src/controllers/Users/LoginUsers.js
+++ src/controllers/Users/LoginUsers.js
@@ -10,15 +10,18 @@ const loginUsers = async (req, res) => {
         password: password,
       },
     });
-
+  
     if (user) {
+      if (user.isBanned) { // Si el usuario está baneado, devuelve un mensaje de error
+        return res.status(403).json({ message: 'Usuario bloqueado. Comuníquese con el administrador.' });
+      }
+      // Si el usuario no está baneado, permite el inicio de sesión
       res.status(200).json({
         message: 'Inicio de sesión exitoso',
         user: {
           id: user.id,
           fullName: user.fullName,
           email: user.email,
-      
         }
       });
     } else {
