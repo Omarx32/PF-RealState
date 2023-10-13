adc9f8cc059e199de883f5d86c60a7b4d0e7d112
fabian <fasalpe34@gmail.ocm>
2023-10-13 10:51:08 -0500

    actualizacion DashboardAllRents

--- src/controllers/DashboardAllRents/DashboardAllRents.js
+++ src/controllers/DashboardAllRents/DashboardAllRents.js
@@ -1,37 +1,48 @@
-const { Reservation } = require("../../db");
+const { Reservation, Users } = require("../../db");
 
 const DashboardAllRents = async () => {
-    try {
-      const reservations = await Reservation.findAll();
-  
-      const monthlyStatistics = {};
-  
-      reservations.forEach((reservation) => {
-        const month = reservation.month; 
-        const price = reservation.price;
-  
-        if (!monthlyStatistics[month]) {
-          monthlyStatistics[month] = {
-            month,
-            numberOfReservations: 0,
-            totalReservationPrice: 0,
-            totalEarnings: 0,
-          };
-        }
-  
-        monthlyStatistics[month].numberOfReservations += 1;
-        monthlyStatistics[month].totalReservationPrice += price;
-        monthlyStatistics[month].totalEarnings += price;
-      });
-  
-      const statisticsArray = Object.values(monthlyStatistics);
-  
-      return statisticsArray;
-    } catch (error) {
-      console.error(error);
-      throw new Error('Error fetching statistics');
-    }
-  };
+  try {
+    const reservations = await Reservation.findAll({
+      include: [
+        {
+          model: Users,
+          attributes: ["id"], 
+        },
+      ],
+    });
+
+    const monthlyStatistics = {};
+
+    reservations.forEach((reservation) => {
+      const month = reservation.month;
+      const price = reservation.price;
+      const userId = reservation.User.id; 
+
+      if (!monthlyStatistics[month]) {
+        monthlyStatistics[month] = {
+          month,
+          numberOfReservations: 0,
+          totalReservationPrice: 0,
+          userIds: [],
+        };
+      }
+
+      monthlyStatistics[month].numberOfReservations += 1;
+      monthlyStatistics[month].totalReservationPrice += price;
+      monthlyStatistics[month].userIds.push(userId); 
+    });
+
+    const statisticsArray = Object.values(monthlyStatistics);
+
+    return statisticsArray;
+  } catch (error) {
+    console.error(error);
+    throw new Error('Error fetching statistics');
+  }
+};
+
+module.exports = { DashboardAllRents };
+
   
 
 module.exports = {DashboardAllRents}
